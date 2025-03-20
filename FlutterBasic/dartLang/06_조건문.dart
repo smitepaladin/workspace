adfb67bb95c41ff5b9e7a0a273309a4183c56ac7
smitepaladin <darktemper20701@gmail.com>
2025-03-20 10:57:41 +0900

    06
    06

--- FlutterBasic/dartLang/06_조건문.dart
+++ FlutterBasic/dartLang/06_조건문.dart
@@ -1,4 +1,15 @@
 main(){
   // 조건문 (Conditinal Statement)
+  int num1 = 5;
+  String result = "";
   
+  if(num1 > 10){ // 이 중괄호 area를 Scope라고 한다.
+    result = "10보단 큰";
+  }else if(num1 < 10){
+    result = "10보다 작은";
+  }else{ // else만 쓰면 Scope가 없다
+    result = "10과 같은";
+  }
+
+print("입력된 숫자 $num1은(는) $result 수 입니다.");
 }
