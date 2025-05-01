3ae72bca268d2faadb7ee111cba4a17df1ff4251
smitepaladin <darktemper20701@gmail.com>
2025-05-02 01:06:44 +0900

    5/2 전종익
    team4 shoeshop model 작성중

--- 4teamshoeshop/team4shoeshop/lib/model/customer.dart
+++ 4teamshoeshop/team4shoeshop/lib/model/customer.dart
@@ -1,13 +1,13 @@
- class Customer{
+class Customer{
   final int? id;
   final String cid;
   final String cname;
   final String cpassword;
-  final String cphone;
+  final int cphone;
   final String cemail;
-  final String ccardnum;
-  final String ccardcvc;
-  final String ccarddate;
+  final int ccardnum;
+  final int ccardcvc;
+  final int ccarddate;
 
   Customer({
     this.id,
@@ -19,11 +19,17 @@
     required this.ccardnum,
     required this.ccardcvc,
     required this.ccarddate
+    }
+  );
 
-
-
-
-
-
-  });
- }
+  Customer.fromMap(Map<String, dynamic> res)
+  : id = res['id'],
+  cid = res['cid'],
+  cname = res['cname'],
+  cpassword = res['cpassword'],
+  cphone = res['cphone'],
+  cemail = res['cemail'],
+  ccardnum = res['ccardnum'],
+  ccardcvc = res['ccardcvc'],
+  ccarddate = res['ccardate'];
+}
